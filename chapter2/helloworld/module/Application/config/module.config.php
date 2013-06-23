442c6144e1507a9eb526905ccfae2189a0759e1b
olegkrivtsov <olegkrivtsov@gmail.com>
2013-06-23 21:45:17 +0700

    added menu helper

--- chapter2/helloworld/module/Application/config/module.config.php
+++ chapter2/helloworld/module/Application/config/module.config.php
@@ -20,6 +20,16 @@ return array(
                     ),
                 ),
             ),
+            'contact' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/',
+                    'defaults' => array(
+                        'controller' => 'Application\Controller\Index',
+                        'action'     => 'contact',
+                    ),
+                ),
+            ),
             // The following is a route to simplify getting started creating
             // new controllers and actions without needing to create a new
             // module. Simply drop new controllers in, and you can access them
---
+++ chapter2/helloworld/module/Application/src/Application/View/Helper/Menu.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * This class displays menu bar.
+ * 
+ */
+class Menu {
+ 
+    /**
+     * Menu items.
+     * @var array 
+     */
+    protected $items;
+    
+    /**
+     * Constructor.
+     * @param array $config
+     */
+    public function __construct($config) {
+        
+        if(isset($config['items'])) {
+            $this->items = $config['items'];
+        } else {
+            throw \Exception('No items specified');
+        }
+        
+    }
+    
+    /**
+     * Returns HTML code of the menu.
+     * @return string
+     */
+    public function __toString() {
+        return $this->render();
+    }
+    
+    /**
+     * Renders the menu.
+     * @return string HTML code of the menu.
+     */
+    public function render() {
+        
+        $result = '<div class="navbar">';
+        $result .= '<div class="navbar-inner">';
+        $result .= '<ul>';
+        
+        foreach($this->items as $item) {
+            $result .= $this->renderItem($item);
+        }
+        
+        $result .= '</ul>';
+        $result .= '</div>';
+        $result .= '</div>';
+        
+        return $result;
+        
+    }
+    
+    /**
+     * Renders an item.
+     * @param array $item
+     * @return string HTML code of the item.
+     */
+    protected function renderItem($item) {
+        
+        $isActive = isset($item['active'])?$item['active']:false;
+        $label = isset($item['label'])?$item['label']:'';
+        $link = isset($item['link'])?$item['link']:'#';
+        
+        $result = $isActive?'<li class="active">':'<li>';
+        $result .= '<a href="'.$link.'">'.$label.'</a>';
+        $result .= '</li>';
+    
+        return $result;
+    }
+    
+    
+}
