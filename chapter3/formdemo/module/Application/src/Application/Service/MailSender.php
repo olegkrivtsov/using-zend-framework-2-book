01fd4550bed4865944c03437926e1d9f2da7e217
olegkrivtsov <olegkrivtsov@gmail.com>
2013-07-04 17:16:26 +0700

    contact us form

--- chapter3/formdemo/module/Application/src/Application/Service/MailSender.php
+++ chapter3/formdemo/module/Application/src/Application/Service/MailSender.php
@@ -2,6 +2,8 @@
 
 namespace Application\Service;
 
+use Zend\Mail;
+
 /**
  * This class is used to deliver an E-mail message to recipient.
  */
@@ -15,7 +17,17 @@ class MailSender {
      * @return boolean
      */
     public function sendMail($recipient, $subject, $text) {
-        
+
+        // Create E-mail message
+        $mail = new Mail\Message();
+        $mail->setBody('This is the text of the email.');
+        $mail->setFrom('Freeaqingme@example.org', 'Sender\'s name');
+        $mail->addTo('Matthew@example.com', 'Name o. recipient');
+        $mail->setSubject('TestSubject');
+
+        // Send E-mail message
+        $transport = new Mail\Transport\Sendmail('-fno-reply@example.com');
+        $transport->send($mail);
         
         return true;
     }
