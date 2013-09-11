7adddb4276d7ee046818760e751ec691b4c45949
Oleg Krivtsov <olegkrivtsov@gmail.com>
2013-09-11 16:44:57 +0700

    improved Menu view helper

--- helloworld/module/Application/src/Application/View/Helper/Menu.php
+++ helloworld/module/Application/src/Application/View/Helper/Menu.php
@@ -81,11 +81,36 @@ class Menu extends \Zend\View\Helper\AbstractHelper {
         $id = isset($item['id']) ? $item['id'] : '';
         $isActive = ($id==$this->activeItemId);
         $label = isset($item['label']) ? $item['label'] : '';
-        $link = isset($item['link']) ? $item['link'] : '#';
         
-        $result = $isActive?'<li class="active">':'<li>';
-        $result .= '<a href="'.$link.'">'.$label.'</a>';
-        $result .= '</li>';
+        $result = ''; 
+        
+        if(isset($item['dropdown'])) {
+            
+            $dropdownItems = $item['dropdown'];
+            
+            $result .= $isActive?'<li class="dropdown active">':'<li class="dropdown">';
+            $result .= '<a href="#" class="dropdown-toggle" data-toggle="dropdown">';
+            $result .= $label . ' <b class="caret"></b>';
+           
+            $result .= '<ul class="dropdown-menu">';
+            foreach($dropdownItems as $item) {
+                $link = isset($item['link']) ? $item['link'] : '#';
+                $label = isset($item['label']) ? $item['label'] : '';
+                
+                $result .= $isActive?'<li class="active">':'<li>';
+                $result .= '<a href="'.$link.'">'.$label.'</a>';
+                $result .= '</li>';
+            }
+            $result .= '</ul>';
+            $result .= '</a>';
+            
+        } else {        
+            $link = isset($item['link']) ? $item['link'] : '#';
+            
+            $result .= $isActive?'<li class="active">':'<li>';
+            $result .= '<a href="'.$link.'">'.$label.'</a>';
+            $result .= '</li>';
+        }
     
         return $result;
     }
